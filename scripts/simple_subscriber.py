87fbd939f8b184f65d4afe23ddcd400e6b5125d3
Cole <cpaulin@mit.edu>
2023-02-16 03:04:21 +0000

    simple_subscriber

--- scripts/simple_subscriber.py
+++ scripts/simple_subscriber.py
@@ -32,27 +32,33 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # Revision $Id$
-import random
 
-## Simple talker demo that published std_msgs/Strings messages
+## Simple talker demo that listens to std_msgs/Strings published 
 ## to the 'chatter' topic
 
 import rospy
-import random
 from std_msgs.msg import Float32
+import numpy as np
 
-def simple_publisher():
-    pub = rospy.Publisher('my_random_float', Float32, queue_size=10)
-    rospy.init_node('simple_publisher', anonymous=True)
-    rate = rospy.Rate(20) # 10hz
-    while not rospy.is_shutdown():
-        num = random.uniform(0,10)
-        rospy.loginfo(num)
-        pub.publish(num)
-        rate.sleep()
+def simple_subscriber():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    pub = rospy.Publisher('random_float_log', Float32, queue_size=10)
+    rospy.init_node('simple_subscriber', anonymous=True)
+
+    def callback(data):
+        n = np.log(data.data)
+        rospy.loginfo(n)
+        pub.publish(n)
+
+    rospy.Subscriber('my_random_float', Float32, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        simple_publisher()
-    except rospy.ROSInterruptException:
-        pass
+    simple_subscriber()
