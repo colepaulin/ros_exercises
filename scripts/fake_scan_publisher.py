c0a0ed2f114e8f46ce6f021f2817db2f29ab53ca
Cole <cpaulin@mit.edu>
2023-02-20 01:36:40 +0000

    Q3 Complex Publisher Commit

---
+++ scripts/fake_scan_publisher.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+## Simple talker demo that published std_msgs/Strings messages
+## to the 'chatter' topic
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from numpy import pi
+import random
+
+def fake_scan_publisher():
+
+    pub = rospy.Publisher('fake_scan_publisher', LaserScan, queue_size=10)
+    rospy.init_node('fake_scan_publisher', anonymous=True)
+    rate = rospy.Rate(20) # 20hz
+
+    while not rospy.is_shutdown():
+
+        laser_scan = LaserScan()
+
+        laser_scan.header.stamp = rospy.get_rostime()
+        laser_scan.header.frame_id = 'base_link'
+        laser_scan.angle_min = rospy.get_param("angle_min",-2.0 * pi / 3.0)
+        laser_scan.angle_max = rospy.get_param("angle_max",2.0 * pi / 3.0)
+        laser_scan.angle_increment = rospy.get_param("angle_increment", 1.0 * pi / 300)
+        laser_scan.scan_time = 1.0 / 20.0
+        laser_scan.range_min = rospy.get_param('range_min', 1.0)
+        laser_scan.range_max = rospy.get_param('range_max', 10.0)
+
+        laser_scan.ranges = []
+        length = int((laser_scan.angle_max - laser_scan.angle_min) / laser_scan.angle_increment + 1)
+
+        for i in range(length):
+            laser_scan.ranges.append(random.uniform(laser_scan.range_min,laser_scan.range_max))
+
+        pub.publish(laser_scan)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        fake_scan_publisher()
+    except rospy.ROSInterruptException:
+        pass
